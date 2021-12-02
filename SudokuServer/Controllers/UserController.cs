ad65c608d954a60a230357bf46c20c8317348455
Yulia B <yuliavorotbert@gmail.com>
2021-12-02 11:49:10 +0200

    first server commit

---
+++ SudokuServer/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SudokuServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SudokuServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("adduser")]
+        public IEnumerable<User> AddUser(string name)
+        {
+            using (var _context = new DataContext())
+            {
+                User user = new User();
+                user.name = name;
+
+                _context.Users.Add(user);
+                _context.SaveChanges();
+
+                return _context.Users.ToList();
+            }
+        }
+    }
+}
---
+++ SudokuServer/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+
+
+namespace SudokuServer.Models
+{
+    public class User 
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public string password { get; set; }
+    }
+}
