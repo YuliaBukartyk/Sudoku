e2714007dddd31c8e2ae3a2a04f0f034d021b7a2
Rotbert, Yulia <yulia.rotbert@intel.com>
2021-12-29 22:29:57 +0200

    Changed the board thickness and add menu game page

--- SudokuClient/ViewModels/EasyLevelGameViewModel.cs
+++ SudokuClient/ViewModels/EasyLevelGameViewModel.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using SudokuClient.Commands;
+using SudokuClient.Services;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace SudokuClient.ViewModels
 {
     public class EasyLevelGameViewModel : BaseViewModel
     {
-        
+        public ICommand BackToMenuCommand { get; }
+
+        public EasyLevelGameViewModel(NavigationService MenuViewNavigationService)
+        {
+            BackToMenuCommand = new NavigateCommand(MenuViewNavigationService);
+        }
+
     }
 }
