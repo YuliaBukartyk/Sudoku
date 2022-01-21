16a90c56ef6353d9f38705727e428eaa85b02e52
valentinanikolenko <valen.niko7@gmail.com>
2022-01-21 18:10:55 +0200

    logic moved to sever and now has defficulty levels

--- SudokuServer/Controllers/BoardController.cs
+++ SudokuServer/Controllers/BoardController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using SudokuServer.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 
@@ -17,7 +19,29 @@ namespace SudokuServer.Controllers
 
         public string GetSudokuBoard()
         {
-            return "023098673079790453680003939993860785950988830606060606454545478787544128541221213";
+            string returnString = "";
+            //int gameMode = 0;
+            List<Board> sudokuBoardList;
+            //string str = "";
+            int listSize = 0;
+            List<int> sudokuList = new List<int>();
+            BoardGenerator sudokuBoard = new BoardGenerator();
+            
+            
+            ////////////////////////////////////////
+            
+            /////////////////////////////////////
+            using (var _context = new sudokuLogic.Models.SudokuDBContext())
+            {
+                sudokuBoardList = _context.Boards.ToList(); // has the contect of the table 
+                listSize = sudokuBoardList.Count();
+                returnString = sudokuBoardList[listSize - 1].sudokuString;// now we have the last sodoku string created.
+                //returnString = sudokuBoardList[listSize - 1].sudokuStringPlayer;// now we have the last sodoku string created.
+                // make the string to be according to level
+                _context.SaveChanges();
+            }
+            //return "023098673079790453680003939993860785950988830606060606454545478787544128541221213";
+            return returnString;
         }
 
         //public IEnumerable<Board> GetSudokuBoard(string level)
--- SudokuServer/Models/Board.cs
+++ SudokuServer/Models/Board.cs
@@ -1,14 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 
 
-
 namespace SudokuServer.Models
 {
-    public class Board 
+    public class Board
     {
+        [Key]
+        public int Id { get; set; }
+
+        //public List<int> sudokuValues;
+
+        public string sudokuString { get; set; } // the sudoku string
+
+        public string sudokuStringPlayer { get; set; } // with zeros to represent empty cells
+
+        // public string sudokuValuesPlayer;
+
+
+
 
     }
 }
